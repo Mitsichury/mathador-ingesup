814c35cfbeba351eaac3dcf6e7afaa3db8607086
blarroqu <b3nit064@hotmail.fr>
2017-01-03 13:20:11 +0100

    Solver + test

---
+++ Jeu du Mathador/Solver/MathadorSolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+    public class MathadorSolver
+    {
+        public static bool IsMathador()
+        {
+            return true;
+        }
+
+        public static string Plus(int a, int b)
+        {
+            string str = a + "+" + b;
+            return str;
+        }
+
+        public static string Moins(int a, int b)
+        {
+            string str = a + "-" + b;
+            return str;
+        }
+
+        public static string Multiply(int a, int b)
+        {
+            string str = a + "*" + b;
+            return str;
+        }
+
+        public static string Divide(int a, int b)
+        {
+            string str = a + "/" + b;
+            return str;
+        }
+
+        public static void Main()
+        {
+
+
+
+        }
+    }
+}
+
---
+++ Jeu du Mathador/SolverTest/MathadorSolverTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver;
+
+namespace SolverTest
+{
+    [TestClass]
+    public class MathadorSolverTest
+    {
+        [TestMethod]
+        public void PlusTest()
+        {
+            string expected = "1+2";
+            string result = MathadorSolver.Plus(1, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void MoinsTest()
+        {
+            string expected = "1-2";
+            string result = MathadorSolver.Moins(1, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            string expected = "1*2";
+            string result = MathadorSolver.Multiply(1, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            string expected = "1/2";
+            string result = MathadorSolver.Divide(1, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
