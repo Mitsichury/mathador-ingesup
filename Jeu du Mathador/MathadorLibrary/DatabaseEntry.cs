986015bde752f4fe96eec837380837e67b49a975
Mitsichury <mitsichury@gmail.com>
2017-01-04 11:31:58 +0100

    Ajout database entry

---
+++ Jeu du Mathador/MathadorLibrary/DatabaseEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathadorLibrary
+{
+    public class DatabaseEntry
+    {
+        public string NomJoueur { get; }
+        public int Points { get; }
+        public int TempsMoyen { get; }
+        public int NombreMatahdor { get; }
+        public int PointsMoyen { get; }
+
+        public DatabaseEntry(string nomJoueur, int points, int tempsMoyen, int nombreMatahdor, int pointsMoyen)
+        {
+            NomJoueur = nomJoueur;
+            Points = points;
+            TempsMoyen = tempsMoyen;
+            NombreMatahdor = nombreMatahdor;
+            PointsMoyen = pointsMoyen;
+        }
+    }
+}
